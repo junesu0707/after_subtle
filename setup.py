d355a58ad24997b11b19e869247b0bff8b78c181
junesu0707 <wnstn9001@gmail.com>
2023-11-01 00:01:37 +0900

    add pyYAML library

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='after_subtle',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=['numpy','pandas','matplotlib','scipy'],
+    install_requires=['numpy','pandas','matplotlib','scipy','PyYAML'],
     python_requires='>=3.7'
 )
