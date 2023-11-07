1438f5a30d31b94c829c540eb71fe58607a0cfc0
junesu0707 <wnstn9001@gmail.com>
2023-11-07 22:06:29 +0900

    params for after_subtle of model_subtle_20231101_090236

---
+++ Project/Broad/model_subtle_20231101_090236/params.py
@@ -0,0 +1,27 @@
+## General
+project_name = 'Broad_gria_setd'
+dataset_name = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-Broad\GRIA3-8_7\dataset1'+r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-Broad\SETD1A-8_7\dataset1'
+fs = 30  # sampling rate of the video data
+
+## subtle
+TRAIN_DIR = r"C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\trainset1.txt"
+TRAIN_SAVE_DIR = r"C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_trainset1"
+
+TEST_DIR = r"C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\testset1.txt"
+TEST_SAVE_DIR = r"C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1"
+
+## after_subtle
+subcluster_num = 77
+
+DIR_gria3 = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Gria3\group_gria3.txt'
+DIR_setd1a = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Setd1a\group_setd1a.txt'
+DIR_gria3_wt = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Gria3\group_gria3_wt.txt'
+DIR_gria3_mut = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Gria3\group_gria3_mut.txt'
+DIR_setd1a_wt = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Setd1a\group_setd1a_wt.txt'
+DIR_setd1a_het = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Setd1a\group_setd1a_het.txt'
+
+SAVE_DIR_gria3 = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Gria3'
+SAVE_DIR_setd1a = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Setd1a'
+
+COORDS_DIR_gria3 = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-Broad\GRIA3-8_7\dataset1'
+COORDS_DIR_setd1a = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-Broad\SETD1A-8_7\dataset1'
