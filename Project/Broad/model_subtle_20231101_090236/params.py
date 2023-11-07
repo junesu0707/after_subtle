adc90d194b150e5ff6ea47ee1b589f789704fac6
junesu0707 <wnstn9001@gmail.com>
2023-11-08 01:35:30 +0900

    d

--- Project/Broad/model_subtle_20231101_090236/params.py
+++ Project/Broad/model_subtle_20231101_090236/params.py
@@ -1,27 +1,22 @@
 ## General
 project_name = 'Broad_gria_setd'
-dataset_name = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-Broad\GRIA3-8_7\dataset1'+r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-Broad\SETD1A-8_7\dataset1'
+# dataset_name = 
 fs = 30  # sampling rate of the video data
 
-## subtle
-TRAIN_DIR = r"C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\trainset1.txt"
-TRAIN_SAVE_DIR = r"C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_trainset1"
-
-TEST_DIR = r"C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\testset1.txt"
-TEST_SAVE_DIR = r"C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1"
-
 ## after_subtle
-subcluster_num = 77
+subcluster_num = 76
 
-DIR_gria3 = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Gria3\group_gria3.txt'
-DIR_setd1a = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Setd1a\group_setd1a.txt'
-DIR_gria3_wt = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Gria3\group_gria3_wt.txt'
-DIR_gria3_mut = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Gria3\group_gria3_mut.txt'
-DIR_setd1a_wt = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Setd1a\group_setd1a_wt.txt'
-DIR_setd1a_het = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Setd1a\group_setd1a_het.txt'
+DIR_gria3_male_wt = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\gria3_male_wt.txt'
+DIR_gria3_male_mut = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\gria3_male_mut.txt'
 
-SAVE_DIR_gria3 = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Gria3'
-SAVE_DIR_setd1a = r'C:\Users\MyPC\Desktop\git\SUBTLE_June\project\Broad\model1\results_testset1\Setd1a'
+DIR_setd1a_male_wt = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\setd1a_male_wt.txt'
+DIR_setd1a_male_het = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\setd1a_male_het.txt'
 
-COORDS_DIR_gria3 = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-Broad\GRIA3-8_7\dataset1'
-COORDS_DIR_setd1a = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-Broad\SETD1A-8_7\dataset1'
+DIR_setd1a_female_wt = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\setd1a_female_wt.txt'
+DIR_setd1a_female_het = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\setd1a_female_het.txt'
+
+SAVE_DIR_gria3_male = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\gria3_male'
+SAVE_DIR_setd1a_male = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\setd1a_male'
+SAVE_DIR_setd1a_female = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\setd1a_female'
+
+COORDS_DIR = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\SETD1A_GRIA3_All(for_analysis)'
