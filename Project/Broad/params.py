fd3e36c26797c6bf7a51cab48108e9de2e945068
junesu0707 <wnstn9001@gmail.com>
2023-11-12 17:41:34 +0900

    update the file path modification

--- Project/Broad/params.py
+++ Project/Broad/params.py
@@ -7,17 +7,17 @@ fs = 30  # sampling rate of the video data
 subcluster_num = 76
 min_occurrence = 0.01   # minimum occurrence of a subcluster to be considered as a behavior
 
-DIR_gria3_male_wt = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\gria3_male_wt.txt'
-DIR_gria3_male_mut = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\gria3_male_mut.txt'
+DIR_gria3_male_wt = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison\gria3_male_wt.txt'
+DIR_gria3_male_mut = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison\gria3_male_mut.txt'
 
-DIR_setd1a_male_wt = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\setd1a_male_wt.txt'
-DIR_setd1a_male_het = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\setd1a_male_het.txt'
+DIR_setd1a_male_wt = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison\setd1a_male_wt.txt'
+DIR_setd1a_male_het = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison\setd1a_male_het.txt'
 
-DIR_setd1a_female_wt = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\setd1a_female_wt.txt'
-DIR_setd1a_female_het = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\setd1a_female_het.txt'
+DIR_setd1a_female_wt = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison\setd1a_female_wt.txt'
+DIR_setd1a_female_het = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison\setd1a_female_het.txt'
 
-SAVE_DIR_gria3_male = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\gria3_male'
-SAVE_DIR_setd1a_male = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\setd1a_male'
-SAVE_DIR_setd1a_female = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\group_comparison\setd1a_female'
+SAVE_DIR_gria3_male = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison\gria3_male'
+SAVE_DIR_setd1a_male = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison\setd1a_male'
+SAVE_DIR_setd1a_female = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison\setd1a_female'
 
-COORDS_DIR = r'C:\Users\MyPC\Desktop\git\after_subtle\after_subtle\Project\Broad\SETD1A_GRIA3_All(for_analysis)'
+COORDS_DIR = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\SETD1A_GRIA3_All(for_analysis)'
