1063ee048b3345cb70cfc0295a5d5cb1adf347b9
junesu0707 <wnstn9001@gmail.com>
2023-12-17 23:38:52 +0900

    params for after_subtle analysis from 'model_subtle_tr_0.4_n_24_20231108_034025'

---
+++ Project/Broad/params2.py
@@ -0,0 +1,25 @@
+## General
+project_name = 'Broad_gria_setd'
+fs = 30  # sampling rate of the video data
+
+## subtle
+model_name = 'model_subtle_tr_0.4_n_24_20231108_034025'
+
+## after_subtle
+subcluster_num = 76
+min_occurrence = 0.01   # minimum occurrence of a subcluster to be considered as a behavior
+
+DIR_gria3_male_wt = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison2\gria3_male_wt.txt'
+DIR_gria3_male_mut = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison2\gria3_male_mut.txt'
+
+DIR_setd1a_male_wt = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison2\setd1a_male_wt.txt'
+DIR_setd1a_male_het = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison2\setd1a_male_het.txt'
+
+DIR_setd1a_female_wt = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison2\setd1a_female_wt.txt'
+DIR_setd1a_female_het = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison2\setd1a_female_het.txt'
+
+SAVE_DIR_gria3_male = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison2\gria3_male'
+SAVE_DIR_setd1a_male = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison2\setd1a_male'
+SAVE_DIR_setd1a_female = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\group_comparison2\setd1a_female'
+
+COORDS_DIR = r'C:\Users\MyPC\Desktop\git\after_subtle\Project\Broad\SETD1A_GRIA3_All(for_analysis)'
