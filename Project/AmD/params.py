28f583dd020f94369f49c232cc75720b1166964d
junesu0707 <wnstn9001@gmail.com>
2024-01-15 11:36:55 +0900

    moved from subtle_June

---
+++ Project/AmD/params.py
@@ -0,0 +1,26 @@
+## General
+project_name = 'AmD'
+dataset_name = 'trainset1'
+fs = 20   # sampling rate of the video data
+
+## subtle_AmD
+TRAIN_DIR = r"C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-AmD_이성민학생\SUBTLE_embedding\trainset1\train.txt"
+TRAIN_SAVE_DIR = r"C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-AmD_이성민학생\SUBTLE_embedding\trainset1\results_train"
+
+TEST_DIR = r"C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-AmD_이성민학생\SUBTLE_embedding\trainset1\test.txt"
+# TEST_SAVE_DIR = 
+
+## after_subtle_AmD
+subcluster_num = 83
+wt_d54 = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-AmD_이성민학생\dataset1\SUBTLE_embedding\trainset1\results_test\wt\d54'
+wt_d57 = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-AmD_이성민학생\dataset1\SUBTLE_embedding\trainset1\results_test\wt\d57'
+wt_d61 = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-AmD_이성민학생\dataset1\SUBTLE_embedding\trainset1\results_test\wt\d61'
+wt_d64 = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-AmD_이성민학생\dataset1\SUBTLE_embedding\trainset1\results_test\wt\d64'
+
+ko_d54 = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-AmD_이성민학생\dataset1\SUBTLE_embedding\trainset1\results_test\ko\d54'
+ko_d57 = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-AmD_이성민학생\dataset1\SUBTLE_embedding\trainset1\results_test\ko\d57'
+ko_d61 = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-AmD_이성민학생\dataset1\SUBTLE_embedding\trainset1\results_test\ko\d61'
+ko_d64 = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-AmD_이성민학생\dataset1\SUBTLE_embedding\trainset1\results_test\ko\d64'
+
+ko_dead_first = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-AmD_이성민학생\dataset1\SUBTLE_embedding\trainset1\results_test\ko\dead_before_d64\record_first'
+ko_dead_last = r'C:\Users\MyPC\Desktop\실험실\2.실험데이터\AVATAR-AmD_이성민학생\dataset1\SUBTLE_embedding\trainset1\results_test\ko\dead_before_d64\record_last'
